7bdaef5a02893cb16f95629bbd926f74e9724900
GM_Finn <votienthien.196@gmail.com>
2025-09-25 18:36:33 +0700

    update library

---
+++ list.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+#include <cfloat>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <string>
+#include <iomanip>      // setprecision, fixed, ...
+#include <numeric>      // accumulate, gcd (C++17+)
+#include <cctype>       // isdigit, tolower, ...
+using namespace std;
+
+int main() {
+    // code here
+    return 0;
+}
